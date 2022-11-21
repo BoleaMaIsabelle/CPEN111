4acbd82dad0f9f8fdb60932a85d6c5e582c1d879
BoleaMa.Isabelle <94947043+BoleaMaIsabelle@users.noreply.github.com>
2022-11-22 05:47:42 +0800

    Update exercise1.m

--- exercise1.m
+++ exercise1.m
@@ -4,22 +4,22 @@ clc
 
 pkg load image;
 
-img = imread('C:\Users\ADMIN\Desktop\4th year\CPEN 111\math\fruits.png');  # Read the image information
-subplot(231);
+img = imread('C:\Users\ADMIN\Desktop\4th year\CPEN 111\math\fruits.png');  # Read the image info
+subplot(131);
 imshow(img);
 title('Original Image')
 
 
-img1 = imresize(imresize(img,1/16),16);
-subplot(232);
+img1 = imresize(imresize(img,1/10),10);
+subplot(132);
 imshow(img1);
-title('at 16x16 resolution');
+title('at 10x10 resolution');
 imwrite(img1,'fruits2.png');
 
 
 chg_color = rgb2hsv(img);    # Change the colore image to HSV
-subplot(233)
-imshow(chg_color);           # Display converted version (HSV)
+subplot(133)
+imshow(chg_color);           # Show HSV 
 title('RGB to HSV');
 imwrite(chg_color,'fruits3.png');
 
